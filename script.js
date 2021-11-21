1f7fdec7338b60e9eae88999f5b035cba5e68d5c
Mateus Zuqui <92697780+zucada@users.noreply.github.com>
2021-11-21 03:35:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const egg = document.querySelector('.egg');
+const container = document.querySelector('.container')
+const textBox = document.querySelector('.text-box')
+
+function brokenEgg() {
+    document.querySelector('.egg').src = "assets/images/friedegg.png"
+    document.querySelector('.hola').innerHTML = "Ahora estoy muertito, gracias!"
+    
+};
+
+
